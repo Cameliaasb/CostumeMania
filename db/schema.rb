51a696f6a6b18ad462a610547fd2ee083f454c55
Anne Flore Bernard <b.anneflore@yahoo.fr>
2023-08-22 17:02:33 +0200

    show costume

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_21_153052) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_22_121421) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "monuments", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.date "opening_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
