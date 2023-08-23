d70d32a8bdb57a45f23de276d7ada4c07b59e174
Yann Le Saint <podologue.brindas@gmail.com>
2023-08-23 14:38:08 +0200

    ajout de photo show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2023_08_23_093442) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,14 +66,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_093442) do
     t.string "gender"
     t.string "age"
     t.index ["user_id"], name: "index_costumes_on_user_id"
-  end
-
-  create_table "monuments", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.date "opening_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
