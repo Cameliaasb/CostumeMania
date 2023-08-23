277023249210058560da00949199fb04c2b398cb
Alexander Llewellyn <alexander.llewellyn.pro@gmail.com>
2023-08-23 10:42:34 +0200

    seeding

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
 
-  has_many :costumes
+  has_many :costumes, dependent: :destroy
   validates :first_name, :last_name, presence: true
 end
