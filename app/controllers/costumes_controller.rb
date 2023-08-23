0a37cff2ac006ac3a9c1ae63e6c1d29ab01fa3de
camelia assobhei <camelia.assobhei@gmail.com>
2023-08-23 11:23:07 +0200

    fixed little bug in show form

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -5,6 +5,7 @@ class CostumesController < ApplicationController
 
   def show
     @costume = Costume.find(params[:id])
+    @booking = Booking.new
   end
 
   def new
--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,7 +1,7 @@
 class Costume < ApplicationRecord
   has_one_attached :photo
   belongs_to :owner, class_name: "User", foreign_key: "user_id"
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
 
   validates :size, :condition, :price, :name, presence: true
 end
