b6e8179d4218881b76477d2b64d951ead6aa22e3
camelia assobhei <camelia.assobhei@gmail.com>
2023-08-24 15:27:38 +0200

    fixed footer, my costumes

--- app/controllers/costumes_controller.rb
+++ app/controllers/costumes_controller.rb
@@ -12,12 +12,10 @@ class CostumesController < ApplicationController
   end
 
   def new
-    # Reste à faire : gérer les authorisation avec devise
     @costume = Costume.new
   end
 
   def create
-    # attention ne marche pas si user non connecté, il faut gérer les authorisation avec devise
     costume = Costume.new(costume_params)
     costume.owner = current_user
     if costume.save
@@ -28,27 +26,24 @@ class CostumesController < ApplicationController
   end
 
   def edit
-    # le bouton ne doit s'afficher que pour le owner
   end
 
   def update
     @costume.update(costume_params)
-    if costume.save
-      redirect_to costume_path(costume)
+    if @costume.save
+      redirect_to costume_path(@costume)
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
-    # le bouton ne doit s'afficher que pour le owner
     @costume.destroy
-    redirect_to restaurants_path, status: :see_other
+    redirect_to costumes_path, status: :see_other
   end
 
   def my_costumes
-    # Reste à faire : gérer les authorisation avec devise
-    @costumes = Costume.where(owner: "current_user")
+    @costumes = Costume.where(owner: current_user)
   end
 
   private
