7175edab6b5356765ed204593d3c5f501b4e0fb1
Yann Le Saint <podologue.brindas@gmail.com>
2023-08-24 16:21:56 +0200

    fix4all

--- db/schema.rb
+++ db/schema.rb
@@ -68,14 +68,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_093442) do
     t.index ["user_id"], name: "index_costumes_on_user_id"
   end
 
-  create_table "monuments", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.date "opening_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
