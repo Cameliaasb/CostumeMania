472b6497a2b4ff0758921a2f7154fdefc1fd1c80
camelia assobhei <camelia.assobhei@gmail.com>
2023-09-17 05:55:58 +0200

    [FILTERS] fixed asset paths]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,13 +4,10 @@ module ApplicationHelper
       link_to request.params.except(type.to_sym), class: "filter-link active" do
         text.to_s
       end
-      # link_to Inferieur à 1h, request.params.except(:duration), class: "filter-link"
     else
       link_to request.params.merge(type.to_sym => value), class: "filter-link" do
         text.to_s
       end
-      # link_to Inferieur à 1h, request.params.merge(duration: "60"), class: "filter-link"
-
     end
   end
 end
