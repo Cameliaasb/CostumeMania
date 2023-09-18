fe0775ce94967eb40ef18e5438cd3651dd5e7a51
camelia assobhei <camelia.assobhei@gmail.com>
2023-09-18 17:28:53 +0200

    [FEAT] Working gender and keyword filters with JS

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -3,21 +3,41 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="search"
 export default class extends Controller {
 
-  static targets = ["keyword", "form", "div"]
+  static targets = ["keyword", "form", "div", "women", "men"]
   static values = {url : String}
 
   connect() {
+    // DOMParser doc + stackoverflow (parse data from html response)
+    this.parser = new DOMParser();
 
   }
 
   keyword() {
-    // DOMParser doc + stackoverflow (parse data from html response)
-    const parser = new DOMParser();
+    console.log("method: keyword")
+    return `${this.keywordTarget.value}`
 
-    // stimulus doc : working with external resources
-    fetch(`/costumes${this.urlValue}${this.keywordTarget.value}`)
-    .then(response => response.text())
-    .then(html => this.divTarget.innerHTML = parser.parseFromString(html, "text/html").getElementById("costumes").innerHTML)
   }
+
+  gender() {
+    console.log("method: gender")
+    return `${(this.womenTarget.checked ? (this.menTarget.checked ? "" : "Women") : (this.menTarget.checked ? "Men" : ""))}`
+
+    // fetch(`${baseUrl}${gender}`)
+    //   .then(response => response.text())
+    //   .then(html => this.divTarget.innerHTML = this.parser.parseFromString(html, "text/html").getElementById("costumes").innerHTML)
+  }
+
+  reload() {
+    // stimulus doc : working with external resources
+    const baseUrl = `${window.location.href}`
+    const url = `${baseUrl}?gender=${this.gender()}&keyword=${this.keyword()}`
+    console.log(url)
+    fetch(url)
+    .then(response => response.text())
+    .then(html => this.divTarget.innerHTML = this.parser.parseFromString(html, "text/html").getElementById("costumes").innerHTML)
+
+  }
+
+
 
 }
