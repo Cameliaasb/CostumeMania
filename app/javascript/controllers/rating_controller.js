abbfd3673f8fb0ab2f8ae57143f04ea237073817
camelia assobhei <camelia.assobhei@gmail.com>
2023-09-20 11:39:15 +0200

    [FRONT] Button to submit review

--- app/javascript/controllers/rating_controller.js
+++ app/javascript/controllers/rating_controller.js
@@ -6,14 +6,17 @@ export default class extends Controller {
   static targets = ["rateInput"]
 
   connect() {
-    this.rateInputTargets.forEach(input => console.log(input.value))
+    this.rateInputTargets.forEach(input => console.log("Inputs => ", input.form))
   }
 
   rate1(e) {
-    console.log(e.target.form)
-    const input = this.rateInputTargets.filter(input => input.form == e.target.form)
-   console.log(input)
-
+    e.preventDefault()
+    // console.log(e.target.closest("form"))
+    const rating = e.target.innerHTML
+    const input = this.rateInputTargets.filter(input => input.form === e.target.closest("form"))
+    // console.log(input)
+    // input[0].value = 1
+    // input[0].form.submit()
   }
 
   rate2(e) {
