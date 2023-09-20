4f3e30ae32adf8b1e7dbfc25cfaca5f0bdd85fd0
camelia assobhei <camelia.assobhei@gmail.com>
2023-09-20 12:48:35 +0200

    [FEAT] Review then redirects to costume page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,7 +10,7 @@ class ReviewsController < ApplicationController
     @review.costume = Costume.find(params[:costume_id])
     @review.user = current_user
     @review.save
-    redirect_to my_bookings_path
+    redirect_to costume_path(@review.costume)
   end
 
   private
