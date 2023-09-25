8ad8dfb72b0bf492fcd9f2412a48acbf21669cdd
camelia assobhei <camelia.assobhei@gmail.com>
2023-09-25 18:58:20 +0200

    [TEST] Root page has link that redirects to costumes index

--- test/system/landingpages_test.rb
+++ test/system/landingpages_test.rb
@@ -3,7 +3,10 @@ require "application_system_test_case"
 class LandingpagesTest < ApplicationSystemTestCase
   test "visiting the landingpage" do
     visit root_url
-
     assert_selector "h2", text: "We got you"
+
+    # Find the "Shop all" link by its text and verify it points to the right page
+    shop_all_link = find("a", text: "Shop all")
+    assert_equal "#{root_url}costumes", shop_all_link[:href]
   end
 end
