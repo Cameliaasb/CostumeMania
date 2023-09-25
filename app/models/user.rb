a4d97f8b1e7b64380eee6b58af1425403ef4d3cc
camelia assobhei <camelia.assobhei@gmail.com>
2023-09-25 11:55:37 +0200

    [BACK] Redirection after log in to fix :back

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :validatable
-
+         :recoverable, :rememberable, :validatable
 
   has_many :bookings, dependent: :destroy
   has_many :costumes, dependent: :destroy
   has_many :reviews, dependent: :destroy
 
-
   validates :first_name, :last_name, presence: true
-
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
-
 end
