3081413448855e1ac655e4b16204eb259e41302f
camelia assobhei <camelia.assobhei@gmail.com>
2023-09-25 11:10:55 +0200

    [GEM] Pick dates with flatpickr

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -5,6 +5,23 @@ import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr";
 export default class extends Controller {
   connect() {
-    flatpickr(this.element)
+    console.log("ici")
+    flatpickr(this.element, {
+      mode: "range",
+      minDate: "today"
+    }
+    )
+
+  }
+
+  convert() {
+      // get hidden inputs (:starts_at, :ends_at)
+    const start_input = this.element.form.elements[1]
+    const end_input = this.element.form.elements[2]
+
+    // // Get the dates from range and give them as values for hidden inputs
+    const dates = this.element.value.split(' to ')
+    if (dates[0]) start_input.value = dates[0]
+    if (dates[1]) end_input.value = dates[1]
   }
 }
