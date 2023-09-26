79b9b943abc53cb0848d294f5a75f946ea968812
camelia assobhei <camelia.assobhei@gmail.com>
2023-09-26 18:18:06 +0200

    [TEST] Core user journer system test

---
+++ test/system/costumes_test.rb
@@ -0,0 +1,37 @@
+require "application_system_test_case"
+
+class CostumesTest < ApplicationSystemTestCase
+  test "A user cannot add new costume if not logged" do
+    visit root_url
+    assert_no_selector ".nav-link", text: "Add a costume"
+  end
+
+  test "A logged user can add a new costume" do
+    # login with devise helper
+    login_as users(:jack)
+    visit root_url
+    # Option to add a costume pops
+    assert_selector ".nav-link", text: "Add a costume"
+    click_on "Add a costume"
+
+    fill_in "costume_name", with: "Test Name"
+    select "S", from: "costume_size"
+    select "unisex", from: "costume_gender"
+    select "Used", from: "costume_condition"
+    fill_in "costume_description", with: "Test description"
+    fill_in "costume_price", with: "1"
+
+    # redirects to costume show
+    click_on "Add a new costume"
+    assert_selector "h2", text: "Test Name"
+
+    # the new costume is in user dashboard
+    visit my_costumes_url
+    assert_selector "h3", text: "Test Name"
+
+    # the new costume isn't in index
+    visit costumes_url
+    assert_no_selector "h3", text: "Test Name"
+
+  end
+end
--- test/system/landingpages_test.rb
+++ test/system/landingpages_test.rb
@@ -1,10 +1,14 @@
 require "application_system_test_case"
 
 class LandingpagesTest < ApplicationSystemTestCase
-  test "visiting the landingpage" do
+  test "Landing page has banner and explanation of concept" do
     visit root_url
-    assert_selector "h2", text: "We got you"
+    assert_selector ".banner", text: "We got you"
+    assert_selector "h3", text: "Concept"
+  end
 
+  test "Banner in landing page has link to index" do
+    visit root_url
     # Find the "Shop all" link by its text and verify it points to the right page
     shop_all_link = find("a", text: "Shop all")
     assert_equal "#{root_url}costumes", shop_all_link[:href]
