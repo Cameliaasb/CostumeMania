16f3c51ae00b44c325513a6a624efd56aa63beb2
camelia assobhei <camelia.assobhei@gmail.com>
2023-09-27 12:58:28 +0200

    [BUG FIX] Dates displayed in input when choosen in new booking (costume show)

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -4,24 +4,21 @@ import { Controller } from "@hotwired/stimulus"
 
 import flatpickr from "flatpickr";
 export default class extends Controller {
+
+  static targets = ["initialInput", "start", "end"]
+
   connect() {
-    console.log("ici")
-    flatpickr(this.element, {
+    flatpickr(this.initialInputTarget, {
       mode: "range",
       minDate: "today"
     }
     )
-
   }
 
   convert() {
-      // get hidden inputs (:starts_at, :ends_at)
-    const start_input = this.element.form.elements[1]
-    const end_input = this.element.form.elements[2]
-
-    // // Get the dates from range and give them as values for hidden inputs
-    const dates = this.element.value.split(' to ')
-    if (dates[0]) start_input.value = dates[0]
-    if (dates[1]) end_input.value = dates[1]
+    // Get the dates from range and give them as values for hidden inputs
+    const dates = this.initialInputTarget.value.split(' to ')
+    if (dates[0]) this.startTarget.value = dates[0]
+    if (dates[1]) this.endTarget.value = dates[1]
   }
 }
