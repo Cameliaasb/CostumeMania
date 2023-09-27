eaaa35e826186da427c5758a71bbc1de66ee57c3
camelia assobhei <camelia.assobhei@gmail.com>
2023-09-27 13:18:05 +0200

    [TEST] Added unit test to booking model to make sure all bookings have the necessary attributes

--- test/models/booking_test.rb
+++ test/models/booking_test.rb
@@ -7,6 +7,16 @@ class BookingTest < ActiveSupport::TestCase
     end
   end
 
+  test "Bookings must all have start and end dates, a client and an owner of costume" do
+    Booking.all.each do |booking|
+      assert booking.start_date, "Booking #{booking.id} doesn't have a start date"
+      assert booking.end_date, "Booking #{booking.id} doesn't have an end date"
+      assert booking.costume, "Booking #{booking.id} doesn't have a costume"
+      assert booking.costume.owner, "Booking #{booking.id} doesn't have a costume owner"
+      assert booking.client, "Booking #{booking.id} doesn't have a client"
+    end
+  end
+
   test "Users can't accept a booking with a starting date in the past" do
     # bookings that were not accepted in the past and with a starting date earlier than today
     Booking.where("start_date < ? AND status != ?", Date.today, "Accepted").each do |booking|
