c9f7a76e6c3b251ec8480215613bca3ecf2d5c40
camelia assobhei <camelia.assobhei@gmail.com>
2023-09-27 11:59:31 +0200

    [TEST] Model test for costumes checks for valid attributes not just presence of attributes

--- test/models/costume_test.rb
+++ test/models/costume_test.rb
@@ -1,14 +1,20 @@
 require "test_helper"
 
 class CostumeTest < ActiveSupport::TestCase
-  test "All costumes have a name, a description, a gender, a condition, a price and a size" do
+  test "should not empty costume" do
+    costume = Costume.new
+    assert_not costume.save, "Saved empty costume"
+  end
+
+  test "All costumes have valid attributes (name / description / photo / price / size / condition / gender)" do
     Costume.all.each do |costume|
-      assert costume.name
-      assert costume.description
-      assert costume.size
-      assert costume.price
-      assert costume.condition
       assert costume.photo
+      assert_instance_of String, costume.name
+      assert_instance_of String, costume.description
+      assert_instance_of Integer, costume.price
+      assert_includes Costume::SIZE, costume.size, "Size not recognized, check for caps or typos"
+      assert_includes Costume::CONDITION, costume.condition, "Condition not recognized, check for caps or typos"
+      assert_includes Costume::GENDER, costume.gender, "Gender not recognized, check for caps or typos"
     end
   end
 end
