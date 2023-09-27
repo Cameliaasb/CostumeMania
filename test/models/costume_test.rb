3d301f296e44be342b366464002b4dbd9247b2b5
camelia assobhei <camelia.assobhei@gmail.com>
2023-09-27 10:58:28 +0200

    [TEST] Added model tests for costumes and users

--- test/models/costume_test.rb
+++ test/models/costume_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class CostumeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "All costumes have a name, a description, a gender, a condition, a price and a size" do
+    Costume.all.each do |costume|
+      assert costume.name
+      assert costume.description
+      assert costume.size
+      assert costume.price
+      assert costume.condition
+      assert costume.photo
+    end
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,11 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "All users have an email, a first_name and a last_name" do
+    User.all.each do |user|
+      assert user.first_name
+      assert user.last_name
+      assert user.email
+    end
+  end
 end
